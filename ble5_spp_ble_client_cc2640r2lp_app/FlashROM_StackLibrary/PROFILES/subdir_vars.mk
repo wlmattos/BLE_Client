755320e150aaa975bc640bec997046c8d1967306
Willian Mattos <wills07@gmail.com>
2018-05-23 17:39:15 -0300

    Primeiro commit

---
+++ ble5_spp_ble_client_cc2640r2lp_app/FlashROM_StackLibrary/PROFILES/subdir_vars.mk
@@ -0,0 +1,26 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+C:/ti/simplelink_cc2640r2_sdk_1_35_00_33/source/ti/ble5stack/profiles/roles/cc26xx/central.c 
+
+C_DEPS += \
+./PROFILES/central.d 
+
+OBJS += \
+./PROFILES/central.obj 
+
+OBJS__QUOTED += \
+"PROFILES\central.obj" 
+
+C_DEPS__QUOTED += \
+"PROFILES\central.d" 
+
+C_SRCS__QUOTED += \
+"C:/ti/simplelink_cc2640r2_sdk_1_35_00_33/source/ti/ble5stack/profiles/roles/cc26xx/central.c" 
+
+
